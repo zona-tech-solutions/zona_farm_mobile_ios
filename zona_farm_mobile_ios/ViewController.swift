df49296ae5f2f5b22e076d3089f82d92384c6c43
Fatih <fatakhillah.khaqo@gmail.com>
2025-03-28 20:57:39 +0700

    Initial Commit

---
+++ zona_farm_mobile_ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  zona_farm_mobile_ios
+//
+//  Created by Fatakhillah Khaqo on 28/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
