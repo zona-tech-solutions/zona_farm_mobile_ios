69a9a7ebeec088fe0a7c47ed28de648fe32089e9
Fatih <fatakhillah.khaqo@gmail.com>
2025-04-11 17:11:32 +0700

    Initial Commit

---
+++ zona_farm_mobile_ios/zona_farm_mobile_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  zona_farm_mobile_iosApp.swift
+//  zona_farm_mobile_ios
+//
+//  Created by Fatakhillah Khaqo on 11/04/25.
+//
+
+import SwiftUI
+
+@main
+struct zona_farm_mobile_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
